5b48c1497a5964c87365e2b02dac1c7d0ca11ece
dunstad <anthony00.blount00@gmail.com>
2019-02-25 09:31:03 +0900

    this should fix apostrophes breaking youtube queries

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Run a recognizer using the Google Assistant Library.
+"""Run a recognizer using the Google Assistant Library with button support.
 
 The Google Assistant Library has direct access to the audio API, so this Python
 code doesn't need to record audio. Hot word detection "OK, Google" is supported.
@@ -27,6 +27,7 @@ It is available for Raspberry Pi 2/3 only; Pi Zero is not supported.
 import logging
 import subprocess
 import sys
+import threading
 
 import aiy.assistant.auth_helpers
 import aiy.assistant.device_helpers
@@ -40,88 +41,128 @@ logging.basicConfig(
     format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
 )
 
-def say(text):
-    say_volume = 10
-    aiy.audio.say(text, volume=say_volume)
 
-def toggle_tv_power():
-    say('Switching TV power.')
-    subprocess.call('node /home/pi/voice-recognizer-raspi/src/tv-control/toggle.js', shell=True)
+class MyAssistant(object):
+    """An assistant that runs in the background.
 
-def play_youtube(query):
-    say('Playing {0}.'.format(query))
-    subprocess.call('node /home/pi/voice-recognizer-raspi/src/tv-control/youtube.js {0}'.format(query), shell=True)
+    The Google Assistant Library event loop blocks the running thread entirely.
+    To support the button trigger, we need to run the event loop in a separate
+    thread. Otherwise, the on_button_pressed() method will never get a chance to
+    be invoked.
+    """
+    def __init__(self):
+        self._task = threading.Thread(target=self._run_task)
+        self._can_start_conversation = False
+        self._assistant = None
 
-def set_volume(volume):
-    say('Setting volume to {0}.'.format(volume))
-    subprocess.call('node /home/pi/voice-recognizer-raspi/src/tv-control/volume.js {0}'.format(volume), shell=True)
+    def start(self):
+        """Starts the assistant.
 
-def power_off_pi():
-    say('Good bye!')
-    subprocess.call('sudo shutdown now', shell=True)
+        Starts the assistant event loop and begin processing events.
+        """
+        self._task.start()
 
+    def _run_task(self):
+        credentials = aiy.assistant.auth_helpers.get_assistant_credentials()
+        model_id, device_id = aiy.assistant.device_helpers.get_ids(credentials)
+        with Assistant(credentials, model_id) as assistant:
+            self._assistant = assistant
+            for event in assistant.start():
+                self._process_event(event)
 
-def reboot_pi():
-    say('See you in a bit!')
-    subprocess.call('sudo reboot', shell=True)
+    def say(self, text):
+        say_volume = 10
+        aiy.audio.say(text, volume=say_volume)
 
+    def toggle_tv_power(self):
+        self.say('Switching TV power.')
+        subprocess.call('node /home/pi/voice-recognizer-raspi/src/tv-control/toggle.js', shell=True)
 
-def say_ip():
-    ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
-    say('My IP address is %s' % ip_address.decode('utf-8'))
+    def play_youtube(self, query):
+        self.say('Playing {0}.'.format(query))
+        subprocess.call('node /home/pi/voice-recognizer-raspi/src/tv-control/youtube.js {0}'.format(query.replace("'", "")), shell=True)
 
+    def set_volume(self, volume):
+        self.say('Setting volume to {0}.'.format(volume))
+        subprocess.call('node /home/pi/voice-recognizer-raspi/src/tv-control/volume.js {0}'.format(volume), shell=True)
 
-def process_event(assistant, event):
-    status_ui = aiy.voicehat.get_status_ui()
-    if event.type == EventType.ON_START_FINISHED:
-        status_ui.status('ready')
-        if sys.stdout.isatty():
-            print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
-
-    elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
-        status_ui.status('listening')
-
-    elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
-        print('You said:', event.args['text'])
-        text = event.args['text'].lower()
-        if text == 'power off':
-            assistant.stop_conversation()
-            power_off_pi()
-        elif text == 'reboot':
-            assistant.stop_conversation()
-            reboot_pi()
-        elif text == 'ip address':
-            assistant.stop_conversation()
-            say_ip()
-        elif all(word in text for word in ['turn', 'tv']):
-            assistant.stop_conversation()
-            toggle_tv_power()
-        elif (text.index('play') if 'play' in text else -1) == 0:
-            assistant.stop_conversation()
-            play_youtube(text[text.index(' ') + 1:])
-        elif all(word in text for word in ['set', 'volume']):
-            assistant.stop_conversation()
-            set_volume(text.split(' ')[-1])
+    def power_off_pi(self):
+        self.say('Good bye!')
+        subprocess.call('sudo shutdown now', shell=True)
 
 
+    def reboot_pi(self):
+        self.say('See you in a bit!')
+        subprocess.call('sudo reboot', shell=True)
 
-    elif event.type == EventType.ON_END_OF_UTTERANCE:
-        status_ui.status('thinking')
 
-    elif event.type == EventType.ON_CONVERSATION_TURN_FINISHED:
-        status_ui.status('ready')
+    def say_ip(self):
+        ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
+        self.say('My IP address is %s' % ip_address.decode('utf-8'))
 
-    elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
-        sys.exit(1)
+
+    def _process_event(self, event):
+        status_ui = aiy.voicehat.get_status_ui()
+        if event.type == EventType.ON_START_FINISHED:
+            status_ui.status('ready')
+            self._can_start_conversation = True
+            # Start the voicehat button trigger.
+            aiy.voicehat.get_button().on_press(self._on_button_pressed)
+            if sys.stdout.isatty():
+                print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
+
+        elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+            self._can_start_conversation = False
+            status_ui.status('listening')
+
+        elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
+            print('You said:', event.args['text'])
+            text = event.args['text'].lower()
+            if text == 'power off':
+                self._assistant.stop_conversation()
+                self.power_off_pi()
+            elif text == 'reboot':
+                self._assistant.stop_conversation()
+                self.reboot_pi()
+            elif text == 'ip address':
+                self._assistant.stop_conversation()
+                self.say_ip()
+            elif all(word in text for word in ['turn', 'tv']):
+                self._assistant.stop_conversation()
+                self.toggle_tv_power()
+            elif (text.index('play') if 'play' in text else -1) == 0:
+                self._assistant.stop_conversation()
+                self.play_youtube(text[text.index(' ') + 1:])
+            elif all(word in text for word in ['set', 'volume']):
+                self._assistant.stop_conversation()
+                self.set_volume(text.split(' ')[-1])
+
+
+
+        elif event.type == EventType.ON_END_OF_UTTERANCE:
+            status_ui.status('thinking')
+
+        elif event.type == EventType.ON_CONVERSATION_TURN_FINISHED:
+            status_ui.status('ready')
+            self._can_start_conversation = True
+
+        elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
+            sys.exit(1)
+
+    def _on_button_pressed(self):
+        # Check if we can start a conversation. 'self._can_start_conversation'
+        # is False when either:
+        # 1. The assistant library is not yet ready; OR
+        # 2. The assistant library is already in a conversation.
+        if self._can_start_conversation:
+            self._assistant.start_conversation()
 
 
 def main():
-    credentials = aiy.assistant.auth_helpers.get_assistant_credentials()
-    model_id, device_id = aiy.assistant.device_helpers.get_ids(credentials)
-    with Assistant(credentials, model_id) as assistant:
-        for event in assistant.start():
-            process_event(assistant, event)
+    MyAssistant().start()
 
 
 if __name__ == '__main__':
     main()
+
+
